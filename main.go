9748a391da6329338fb29fa8dee8908b065c2709
persona-mp3 <randomnobscurebs@gmail.com>
2026-03-11 23:34:46 +0000

    chore: working on manager

--- main.go
+++ main.go
@@ -67,7 +67,7 @@ func main() {
 	var serverPort int
 	var mode bool
 	var secure bool
-	flag.IntVar(&serverPort, "port", 4900, "Port to run server on, by defaut runs on 4900")
+	flag.IntVar(&serverPort, "port", 4000, "Port to run server on, by defaut runs on 4900")
 	flag.BoolVar(&mode, "mode", false, "Run the application in dev or prod, default is dev. Specific variables in .env file in root")
 	flag.BoolVar(&secure, "secure", false, "To run the server using TLS or no encryption algorithm")
 	flag.Parse()
